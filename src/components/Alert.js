362a06cb130b8a2acb4447f727b7ad1d65f6dd6d
JiaYu <alice49885@gmail.com>
2024-01-01 00:44:56 +0800

    feat: 🎸 add alert

---
+++ src/components/Alert.js
@@ -0,0 +1,39 @@
+import Swal from "sweetalert2";
+const successToast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 1500,
+  icon: "success",
+});
+const confirmSwal = Swal.mixin({
+  showCancelButton: true,
+  confirmButtonColor: "#1CC8EE",
+  cancelButtonColor: "#414242",
+  confirmButtonText: "確定",
+});
+const errorSwal = Swal.mixin({
+  icon: "error",
+  confirmButtonColor: "#1CC8EE",
+  confirmButtonText: "好的",
+});
+
+const myAlert = {
+  miniToast(title) {
+    return successToast.fire({
+      title,
+    });
+  },
+  confirmModal(title) {
+    return confirmSwal.fire({
+      title,
+    });
+  },
+  errorModal() {
+    return errorSwal.fire({
+      html: `發生錯誤，請稍後再試！`,
+    });
+  },
+};
+
+export { myAlert };
