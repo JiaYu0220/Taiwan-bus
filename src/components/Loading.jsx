8553261c4f1f3ff38beda47943e42ae3ec175dc4
JiaYu <alice49885@gmail.com>
2024-01-01 01:47:50 +0800

    fix: 🐛 img path

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="d-flex flex-grow-1 justify-content-center align-items-center h-100">
-      <img src="../../Spinner.svg" alt="" />
+      <img src="Spinner.svg" alt="loading" />
     </div>
   );
 };
