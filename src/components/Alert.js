ed07dda2d56e946539362008723dc36a8009536c
JiaYu <alice49885@gmail.com>
2024-01-10 00:31:05 +0800

    refactor: 💡 change timer

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,7 +3,7 @@ const successToast = Swal.mixin({
   toast: true,
   position: "top-end",
   showConfirmButton: false,
-  timer: 1500,
+  timer: 1000,
   icon: "success",
 });
 const confirmSwal = Swal.mixin({
